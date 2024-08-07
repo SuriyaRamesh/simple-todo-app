4337feb35a3a98c9454160883673e697d32a9ccd
SuriyaRamesh <suriya.ram@anywhere.co>
2024-08-07 18:53:13 +0530

    add home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,18 @@ export const getAllTodos = async () => {
   return todos;
 };
 
-
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks);
   return (
-    <main>
-      <h1>Demo</h1>
+    <main className="max-w-4xl mx-auto mt-4">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">ToDo List</h1>
+        {/* <AddTask /> */}
+      </div>
+      <div className="text-center">
+        {/* <TodoList tasks={tasks} /> */}
+      </div>
     </main>
   );
 }
